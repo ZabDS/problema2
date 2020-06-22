ce70f0128afb64a0d07682dddfe09a3e2fd570e2
ZabDS <zabdiel1297@gmail.com>
2020-06-22 02:47:46 -0500

    Chat con mp

--- firstnode.py
+++ firstnode.py
@@ -2,42 +2,60 @@ import sys
 import time
 import socket
 import threading
+import json
 sys.path.insert(0, '..') # Import the files where the modules are located
 from p2pnetwork.node import Node
 
 HOST = "localhost"
-PORT = 9100
+PORT = 8300
 
 global clients
+clients=[]
 
-clients={}
+def getNameNode(nodeID):
+    for client in clients:
+        if client['id']== nodeID:
+            return client['name']
+
+def sendMsgToAll(msg,nodeID,mainNode):
+    for client in clients:
+        if client['id'] == nodeID:
+            for node in mainNode.all_nodes:
+                if node.id != nodeID:
+                    mainNode.send_to_node(node,client['name']+": "+msg+"\nInserte Mensaje:")
+            break
+
+def sendMsgToOne(msg,nodeName,mainNode,nameSend):
+    for client in clients:
+        if client['name'] == nodeName:
+            for node in mainNode.all_nodes:
+                if node.id == client['id']:
+                    mainNode.send_to_node(node,nameSend+"(privado): "+msg+"\nInserte Mensaje:")
+                    return True
+    return False        
+                
 
 def node_callback(event, main_node, connected_node, data):
     try:
         if event != 'node_request_to_stop': # node_request_to_stop does not have any connected_node, while it is the main_node that is stopping!
-            print('Event: {} from main node {}: connected node {}: {}'.format(event, main_node.id, connected_node.id, data))
-            if event == 'node_message':
-                main_node.send_to_nodes(data)
+            #print('Event: {} from main node {}: connected node {}: {}'.format(event, main_node.id, connected_node.id, data))
+            if event == 'inbound_node_connected':
+                print("Enviando tabla de clientes:")
+                #main_node.send_to_nodes((json.dumps(clients)))
+            elif event == 'node_message':
+                aux=data.split('&')
+                if(aux[0]=="%?FMSG"):
+                    Daux={'ip':aux[1],'port':aux[2],'id':connected_node.id,'name':aux[3]}
+                    clients.append(Daux)
+                elif(aux[0]=="%?DMSG"):
+                    name1=getNameNode(connected_node.id)
+                    if(sendMsgToOne(aux[4],aux[3],main_node,name1)):
+                        print("Mensaje privado enviado")
+                else:
+                    sendMsgToAll(data,connected_node.id,main_node)
+
     except Exception as e:
         print(e)
-
-def rcvNode(Node):
-    serveraddr=(HOST,PORT+1)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPServerSocket:
-        TCPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        TCPServerSocket.bind(serveraddr)
-        TCPServerSocket.listen(5)
-        try:
-             while True:
-                Client_conn, Client_addr = TCPServerSocket.accept()
-                with Client_conn:
-                    data = Client_conn.recv(1024)
-                    print("Recibiendo nodo, mandando información necesaria para conexión")
-                    msg = str(PORT+len(Node.all_nodes)+2)
-                    Client_conn.sendall(msg.encode())
-        except Exception as e:
-            print (e)        
-
 
 print("First node")
 
@@ -45,13 +63,6 @@ print("First node")
 node_1 = Node(HOST, PORT, node_callback)
 node_1.start()
 
-rcvNodes = threading.Thread(target=rcvNode,args=(node_1,), name='ServirFE')
-rcvNodes.start()
-
-#node_1.stop()
-rcvNodes.join()
-
-
-
+print(node_1.name)
 print('end')
 
--- node.py
+++ node.py
@@ -1,40 +1,63 @@
 import socket
 import os
+import time
+import random
+import json
 import sys
 
 sys.path.insert(0, '..') # Import the files where the modules are located
 from p2pnetwork.node import Node
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 9101  # The port used by the server
+PORT = 8300  # The port used by the server
 buffer_size = 1024
 
-def getPort():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b'Hello')
-        data = s.recv(1024)        
-    s.close()
-    return int(data.decode())
+TClients=[]
+
+def checkSnode(node):
+    if node.host == HOST and node.port == PORT:
+        return True
+    else:
+        return False
+
+def Snode(node):
+    for n in node.all_nodes:
+        if n.host == HOST and n.port == PORT:
+            return n
+
+def connectWAll(clients,node):
+    for client in clients:
+        if client['port'] != node.port:
+            print("Estableciendo conexion con: "+str(client['ip'])+':'+str(client['port']))
+            node.connect_with_node(client['ip'],client['port'])
+
 
 def node_callback(event, main_node, connected_node, data):
     try:
         if event != 'node_request_to_stop': # node_request_to_stop does not have any connected_node, while it is the main_node that is stopping!
             #print('Event: {} from main node {}: connected node {}: {}'.format(event, main_node.id, connected_node.id, data))
             if event == 'node_message':
-                print(data)
-
+                print(data)                            
     except Exception as e:
         print(e)
 
-port = getPort()
-node = Node(HOST,port, node_callback)
+node = Node(socket.gethostbyname(socket.gethostname()),PORT+random.randrange(100), node_callback)
+Nickname = input("Ingrese su nombre: ") 
 node.start()
-node.connect_with_node('127.0.0.1', PORT-1)
-
-Nickname = input("Ingrese su nombre: ")
+node.connect_with_node(HOST, PORT)
+print("host: ",node.host,":",node.port)
+node.send_to_node(Snode(node),'%?FMSG&'+node.host+'&'+str(node.port)+"&"+Nickname)
 while True:
-    opc = int(input("Ingrese una opcion: "))
-    if opc==1:
-        msg = input("Ingrese un mensaje: ")
-        node.send_to_nodes(Nickname+" dice: "+msg)
+    msg = input("Ingrese mensaje: \n")
+    
+    if msg=="!MD":
+        recvNode = input("Ingrese el nombre: ")
+        msg = input("Ingrese el mensaje: ")
+        node.send_to_node(Snode(node),'%?DMSG&'+node.host+'&'+str(node.port)+"&"+recvNode+"&"+msg)
+    else:
+        node.send_to_nodes(msg)
+        os.system("clear")
+
+node.disconnect_with_node(Snode(node))
+node.stop()
+
